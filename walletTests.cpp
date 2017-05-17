62078ddd40adeaba70d97c96b8a5426e263e05c5
Aaron Jaramillo <aajaramillo95@gmail.com>
2017-05-17 03:13:23 -0600

    Test Mnemonic to Master Key

--- walletTests.cpp
+++ walletTests.cpp
@@ -9,8 +9,18 @@ void genericConstructor()
 	wallet.setCoin(1);
 	wallet.dumpKeys();
 }
-
+void testMnemonic_MasterKey()
+{	
+	wallet::hd_private master(to_chunk(wallet::decode_mnemonic(split("label stick flat innocent brother frost rebel aim creek six baby copper need side cannon student announce alpha"))));
+	HD_Wallet wallet("label stick flat innocent brother frost rebel aim creek six baby copper need side cannon student announce alpha");
+	if(master == wallet.getMasterKey()){
+		std::cout << "Success!" << std::endl;
+		std::cout << master.encoded() << std::endl;
+		wallet.displayMasterKey();
+	}
+}
 int main()
 {
-	genericConstructor();
+	//genericConstructor();
+	testMnemonic_MasterKey();
 }
