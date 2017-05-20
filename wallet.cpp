c5977457149420b1e4987474437ba1683e86ff0a
Aaron Jaramillo <aajaramillo95@gmail.com>
2017-05-20 14:45:42 -0600

    Added interface for importing a wallet of coin x, interface for generating wallet of  coin x

--- wallet.cpp
+++ wallet.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <iostream>
 #include "HD_Wallet.cpp"
+#include <sstream>
 
 using namespace bc;
 
@@ -30,37 +31,86 @@ void displayMenu()
 }
 int getInput()
 {
-	int choice;
-	std::cout << "Enter a Number: "; 
-	std::cin >> choice;
+	std::string input;
+	int choice = 0;
+
+	while (true) {
+		std::cout << "Enter a Number: "; 
+		std::getline(cin, input);
+
+		// This code converts from string to number safely.
+		std::stringstream myStream(input);
+		if (myStream >> choice)
+		 break;
+		std::cout << "Invalid number, please try again" << std::endl;
+	}
 	return choice;
+		
 }
 std::string getMnemonic()
 {
+	
 	std::string mnemonicSeed;
-	std::cout << "Enter Seed String:";
-	cin.ignore();
+	std::cout << "Enter Seed String: ";
 	std::getline(cin, mnemonicSeed);
+
 	return mnemonicSeed;
 }
 
+std::string getCoinTicker()
+{
+	
+	std::string ticker;
+	std::cout << "Enter Coin Ticker: ";
+	std::getline(cin, ticker);
+	return ticker;
+}
+Prefixes matchPrefixTicker(std::string coin)
+{
+
+	if(coin == "BTC")
+	{
+		return HD_Wallet().BTC;
+	} else if(coin == "tBTC"){
+
+		return HD_Wallet().tBTC;
+	}else if(coin == "LTC"){
+
+		return HD_Wallet().LTC;
+
+	} else if(coin == "tLTC"){
+
+		return HD_Wallet().tLTC;
+	} else {
+		
+		return HD_Wallet().BTC;
+
+	}
+}
+
 int main(){
-	HD_Wallet wallet = HD_Wallet();
+	HD_Wallet wallet;
 	int choice = 0;
 	while(choice != 4)
 	{
 		mainMenu();
 		choice = getInput();
+
 		if(choice == 1)
 		{	
-			wallet = HD_Wallet();
+			std::string coinInput = getCoinTicker();
+			Prefixes coin = matchPrefixTicker(coinInput);
+			wallet = HD_Wallet(coin);
+ 
 			wallet.dumpKeys();
 		} else if(choice == 2)
 		{	
 			std::string mnemonicSeedList = getMnemonic();
 			if(wallet::validate_mnemonic(split(mnemonicSeedList)))
 			{
-				wallet = HD_Wallet(mnemonicSeedList);
+
+				std::string coinInput = getCoinTicker();
+				wallet = HD_Wallet(mnemonicSeedList, matchPrefixTicker(coinInput));
 				wallet.dumpKeys();
 			} else {
 				std::cout << "Mnemonic Invalid!" << std::endl;
@@ -71,7 +121,7 @@ int main(){
 			while(choice != 8){
 				displayMenu();
 				choice = getInput();
-				std::cin.ignore();
+				//std::cin.ignore();
 				int index;
 				
 				if(choice == 1)
