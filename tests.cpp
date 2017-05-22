b136a28edf13b356f42b9b1b2b53fdd86527c19c
Aaron Jaramillo <aajaramillo95@gmail.com>
2017-05-22 13:59:48 -0600

    keychain setter tests added

--- tests.cpp
+++ tests.cpp
@@ -12,7 +12,7 @@ BOOST_AUTO_TEST_SUITE(hd_wallet_tests)
 #define UNINITIALIZED_ADDRESS "1111111111111111111114oLvT2"
 #define CHILD_ADDRESS_0 "1KD186Ko4ejaBeYk8EpMikYqfxnpnVTYKk"
 #define COIN256_CHILD_ADDRESS_0 "16HAUSw9YhTXu1YPc1RNBeWo64SDc77WHu"
-
+#define ACCOUNT256_CHILD_ADDRESS_0 "1q6Z5ZpsFaqV8VE6LbwKP9uqfPoKQGUQv"
 const Prefixes coin = {256, 0x0488ADE4, 0x0488B21E, 0x00, 0x05};
 const data_chunk seed = to_chunk(wallet::decode_mnemonic(split(MNEMONIC_SEED)));
 
@@ -57,5 +57,54 @@ BOOST_AUTO_TEST_CASE(hd_wallet_construct_mnemonic_coin)
 	BOOST_REQUIRE_EQUAL(wallet.childAddress(0).encoded(), COIN256_CHILD_ADDRESS_0);
 	BOOST_REQUIRE_EQUAL(wallet.getCoinPrefixes().bip44_code, coin.bip44_code);
 }
+BOOST_AUTO_TEST_CASE(hd_wallet_set_coin)
+{
+	HD_Wallet wallet(MNEMONIC_SEED);
+	wallet.setCoin(256);
+	BOOST_REQUIRE_EQUAL(wallet.getCoinPrefixes().bip44_code, 256);
+}
+BOOST_AUTO_TEST_CASE(hd_wallet_set_coin_prefixes)
+{
+	HD_Wallet wallet(MNEMONIC_SEED);
+	wallet.setCoinPrefixes(coin);
+	BOOST_REQUIRE_EQUAL(wallet.childAddress(0).encoded(), COIN256_CHILD_ADDRESS_0);
+	BOOST_REQUIRE_EQUAL(wallet.getCoinPrefixes().bip44_code, coin.bip44_code);
+
+}
+BOOST_AUTO_TEST_CASE(hd_wallet_set_account)
+{
+	HD_Wallet wallet(MNEMONIC_SEED);
+	wallet.set_account(256);
+	BOOST_REQUIRE_EQUAL(wallet.getCurrentAccount(), 256);
+	BOOST_REQUIRE_EQUAL(wallet.childAddress(0).encoded(), ACCOUNT256_CHILD_ADDRESS_0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 BOOST_AUTO_TEST_SUITE_END()
